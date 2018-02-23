e2cca79726a9fe8f6427bc3c686e10a334d89993
LuffyKing <aderinwale17@gmail.com>
2018-02-23 15:11:51 +0100

    html, css and javascript execirse

---
+++ google.js
@@ -0,0 +1,23 @@
+'use strict';
+window.addEventListener('load', function() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  let forms = document.getElementsByClassName('needs-validation');
+  // Loop over them and prevent submission
+  let validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      else{
+        alert(`This is what you typed ${form['query'].value}`);
+      }
+      form.classList.add('was-validated');
+
+
+
+
+    }, false);
+  });
+
+}, false);
